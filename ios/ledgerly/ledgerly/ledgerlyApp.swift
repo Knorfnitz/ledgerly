e63f40ffda73a88e00949f1f948730ab54c9e1cd
Alexander Hasecke <knorfnitz@gmail.com>
2026-01-29 13:32:59 +0100

    chore(ios): initial project scaffolding

---
+++ ios/ledgerly/ledgerly/ledgerlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ledgerlyApp.swift
+//  ledgerly
+//
+//  Created by Alexander Hasecke on 29.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ledgerlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
